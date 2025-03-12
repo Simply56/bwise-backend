7a710ee53ea9b539a9592ccae60c1094b9aae785
Martin Srsen <xsrsen1@fi.muni.cz>
2025-03-12 18:13:42 +0100

    refactoring and add load_raw_json method

--- entities.py
+++ entities.py
@@ -10,6 +10,7 @@ class Jsonable:
         try:
             with open(file_path, "r", encoding="utf-8") as file:
                 data: list = json.load(file)
+
         except (FileNotFoundError, json.JSONDecodeError):
             data = []
 
@@ -18,47 +19,56 @@ class Jsonable:
         with open(file_path, "w", encoding="utf-8") as file:
             json.dump(data, file, indent=4)
 
+    def load_raw_json(self, file_path: str) -> list[dict]:
+        try:
+            with open(file_path, "r", encoding="utf-8") as file:
+                raw_data: list[dict] = json.load(file)
+
+        except (FileNotFoundError, json.JSONDecodeError):
+            raw_data = []
+        return raw_data
+
 
 class User(Jsonable):
-    def __init__(self, username="", data=None):
-        super().__init__()
+    FILE_NAME = "users.json"
 
+    def __init__(self, username="", data=None):
+        data = data or {}  # Ensure data is a dictionary
         self.username = str(data.get("username", username))
 
     def store(self):
-        return super().store("users.json")
+        return super().store(User.FILE_NAME)
 
 
 class Group(Jsonable):
-    def __init__(self, group_name="", creator="", data=None):
-        super().__init__()
+    FILE_NAME = "groups.json"
 
+    def __init__(self, group_name="", creator="", data=None):
         data = data or {}  # Ensure data is a dictionary
         self.group_name = str(data.get("group_name", group_name))
         self.creator = str(data.get("creator", creator))
 
     def store(self):
-        return super().store("groups.json")
+        return super().store(Group.FILE_NAME)
 
 
 class Membership(Jsonable):
-    def __init__(self, username="", group_name="", data=None):
-        super().__init__()
+    FILE_NAME = "memberships.json"
 
+    def __init__(self, username="", group_name="", data=None):
         data = data or {}  # Ensure data is a dictionary
         self.username = str(data.get("username", username))
         self.group_name = str(data.get("group_name", group_name))
 
     def store(self):
-        return super().store("memberships.json")
+        return super().store(Membership.FILE_NAME)
 
 
 class Transaction(Jsonable):
+    FILE_NAME = "transactions.json"
     LAST_ID: int = 0
 
     def __init__(self, group="", payer="", recipient="", amount=0.0, data=None):
-        super().__init__()
-
         Transaction.LAST_ID += 1
         self.id: int = Transaction.LAST_ID
 
@@ -69,4 +79,4 @@ class Transaction(Jsonable):
         self.amount = float(data.get("amount", amount))
 
     def store(self):
-        return super().store("expenses.json")
+        return super().store(Transaction.FILE_NAME)
