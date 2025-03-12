2739498828ce72df3f020ea2ecb275e29508c82b
Martin Srsen <xsrsen1@fi.muni.cz>
2025-03-12 20:39:43 +0100

    add good_to_store method that verifies if object instance can be inserted into the 'database'

--- entities.py
+++ entities.py
@@ -26,33 +26,36 @@ class Jsonable:
 
 class User(Jsonable):
     FILE_NAME = "users.json"
-    STORAGE: list = []
+    STORAGE: list["User"] = []
 
     def __init__(self, username="", data=None):
         data = data or {}  # Ensure data is a dictionary
         self.username = str(data.get("username", username))
 
-    def store(self):
-        return super().store(User.FILE_NAME)
-
     @staticmethod
-    def load(storage: list):
+    def load():
         raw_data = Jsonable.load_raw_json(User.FILE_NAME)
         for raw_json_dict in raw_data:
             User.STORAGE.append(User(data=raw_json_dict))
 
+    def store(self):
+        return super().store(User.FILE_NAME)
+
+    def good_to_store(self) -> bool:
+        for user in User.STORAGE:
+            if user.username == self.username:
+                return False  # user already exists
+        return True
+
 
 class Group(Jsonable):
     FILE_NAME = "groups.json"
-    STORAGE: list = []
+    STORAGE: list["Group"] = []
 
     def __init__(self, group_name="", creator="", data=None):
         data = data or {}  # Ensure data is a dictionary
         self.group_name = str(data.get("group_name", group_name))
         self.creator = str(data.get("creator", creator))
-
-    def store(self):
-        return super().store(Group.FILE_NAME)
 
     @staticmethod
     def load():
@@ -60,18 +63,36 @@ class Group(Jsonable):
         for raw_json_dict in raw_data:
             Group.STORAGE.append(Group(data=raw_json_dict))
 
+    def store(self):
+        return super().store(Group.FILE_NAME)
+
+    def good_to_store(self):
+        if not any(lambda u: u == self.creator, User.STORAGE):
+            return False  # user not found
+        for group in Group.STORAGE:
+            if group.group_name == self.group_name:
+                return False  # group_name is taken
+        return True
+
 
 class Membership(Jsonable):
     FILE_NAME = "memberships.json"
-    STORAGE: list = []
+    STORAGE: list["Membership"] = []
 
     def __init__(self, username="", group_name="", data=None):
         data = data or {}  # Ensure data is a dictionary
         self.username = str(data.get("username", username))
         self.group_name = str(data.get("group_name", group_name))
 
-    def store(self):
-        return super().store(Membership.FILE_NAME)
+    @staticmethod  # TODO: THIS SMELLS BAD
+    def get_members(group_name: str) -> list[User]:
+        result = []
+        for membership in Membership.STORAGE:
+            if membership.group_name == group_name:
+                for user in User.STORAGE:
+                    if user.username == membership.username:
+                        result.append(user)
+        return result
 
     @staticmethod
     def load():
@@ -79,27 +100,62 @@ class Membership(Jsonable):
         for raw_json_dict in raw_data:
             Membership.STORAGE.append(Membership(data=raw_json_dict))
 
+    def store(self):
+        return super().store(Membership.FILE_NAME)
+
+    def good_to_store(self):
+        if not any(lambda u: u.username == self.username, User.STORAGE):
+            return False  # user not found
+        if not any(lambda g: g.group_name == self.group_name, Group.STORAGE):
+            return False  # group not found
+        return True
+
 
 class Transaction(Jsonable):
     FILE_NAME = "transactions.json"
     LAST_ID: int = 0
-    STORAGE: list = []
+    STORAGE: list["Transaction"] = []
 
-    def __init__(self, group="", payer="", recipient="", amount=0.0, data=None):
+    def __init__(self, group_name="", payer="", recipient="", amount=0.0, data=None):
         Transaction.LAST_ID += 1
         self.id: int = Transaction.LAST_ID
 
         data = data or {}
-        self.group = str(data.get("group", group))
+        self.group_name = str(data.get("group_name", group_name))
         self.payer = str(data.get("payer", payer))
         self.recipient = str(data.get("recipient", recipient))
         self.amount = float(data.get("amount", amount))
-
-    def store(self):
-        return super().store(Transaction.FILE_NAME)
 
     @staticmethod
     def load():
         raw_data = Jsonable.load_raw_json(Transaction.FILE_NAME)
         for raw_json_dict in raw_data:
             Transaction.STORAGE.append(Transaction(data=raw_json_dict))
+
+    def store(self):
+        return super().store(Transaction.FILE_NAME)
+
+    def good_to_store(self):
+        for transaction in Transaction.STORAGE:
+            if transaction.id == self.id:
+                return False  # id already used
+
+        if not any(lambda g: g.group_name == self.group_name, Group.STORAGE):
+            return False  # group not found
+        if not any(
+            lambda u: u.username == self.payer, Membership.get_members(self.group_name)
+        ):
+            return False  # payer not a member
+        if not any(
+            lambda u: u.username == self.recipient,
+            Membership.get_members(self.group_name),
+        ):
+            return False  # recipeint not a member
+
+        return True
+
+
+# u1 = User("1")
+# User.STORAGE.append(u1)
+# u2 = User("2")
+# print(u2.good_to_store())
